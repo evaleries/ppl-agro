f10ba540807df6ee48a208c442f1c390a28ac0e9
evaleries <didaarda15@gmail.com>
2020-11-23 17:50:55 +0700

    routes 🛣

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,29 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes([
     'verify' => true,
     'reset' => true,
 ]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+// Authenticated
+Route::group(['middleware' => 'auth'], function () {
+
+    // Admin
+    Route::group(['middleware' => 'role:admin', 'namespace' => 'Admin', 'prefix' => 'admin', 'as' => 'admin.'], function () {
+        Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+        Route::resource('users', 'UserController');
+    });
+
+    // Seller
+    Route::group(['middleware' => 'role:seller', 'namespace' => 'Seller', 'prefix' => 'seller', 'as' => 'seller.'], function () {
+       Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+    });
+
+    // User
+    Route::group(['middleware' => 'role:user', 'namespace' => 'User', 'prefix' => 'my', 'as' => 'user.'], function () {
+        Route::get('orders', 'UserController@orders')->name('orders');
+    });
+});
