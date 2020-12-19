221c7122255539b49504326e4fcfce9092e6cf32
evaleries <didaarda15@gmail.com>
2020-12-19 11:12:28 +0700

    Fix query

--- app/Http/Controllers/Seller/DashboardController.php
+++ app/Http/Controllers/Seller/DashboardController.php
@@ -9,6 +9,7 @@ use App\Models\Order;
 use App\Models\Product;
 use App\Models\StoreBalance;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -18,12 +19,18 @@ class DashboardController extends Controller
         abort_if($community === null || $community->store->id === null, 403, 'Anda tidak memiliki komunitas atau toko!');
 
         $store = $community->store;
-        $orders = Order::with('user')->where('store_id', $store->id)->where('status', Order::STATUS_PROCESSING)->orWhere('status', Order::STATUS_COMPLETED)->orWhere('status', Order::STATUS_ON_DELIVERY);
+        $orders = Order::with('user')
+            ->where('store_id', $store->id)
+            ->where(function ($query) {
+                $query->orWhere('status', Order::STATUS_PROCESSING)
+                    ->orWhere('status', Order::STATUS_COMPLETED)
+                    ->orWhere('status', Order::STATUS_ON_DELIVERY);
+            });
         return view('seller.dashboard', [
             'total_orders' => $orders->count(),
-            'total_products' => Product::where('store_id', $store->id)->count(),
-            'total_events' => CommunityEvent::where('community_id', $community->id)->count(),
-            'total_members' => CommunityMember::where('community_id', $community->id)->count(),
+            'total_products' => Product::where('store_id', $store->id)->get()->count(),
+            'total_events' => CommunityEvent::where('community_id', $community->id)->get()->count(),
+            'total_members' => CommunityMember::where('community_id', $community->id)->get()->count(),
             'store' => $store,
             'balance' => $store->balance,
             'community' => $community,
